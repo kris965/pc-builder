f9c419d027aeda92ee7259e9277eb391a7d26a18
kris965 <kristian_hoogland@hotmail.com>
2020-07-05 12:14:18 +0000

    fixed spelling error flask_pymongo import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import flask_pymongo
-from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 
