9a4548cc56bbadc6a17baa6de4fcdbce9fd8236e
kris965 <kristian_hoogland@hotmail.com>
2020-07-11 12:53:59 +0000

    added edit_build function

--- app.py
+++ app.py
@@ -15,26 +15,26 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 
-@app.route('/')
 @app.route('/all_builds')
 def all_builds():
-    return render_template("allbuilds.html", build=mongo.db.build.find())
+    return render_template("allbuilds.html", builds=mongo.db.build.find())
 
 # Add build form
 
 
+@app.route('/')
 @app.route('/add_build')
 def add_build():
-    build_name = mongo.db.build_name.find()
-    motherboard = mongo.db.motherboard.find()
-    processor = mongo.db.processor.find()
-    processorcooler = mongo.db.processorcooler.find()
+    build_names = mongo.db.build.find()
+    motherboards = mongo.db.motherboard.find()
+    processors = mongo.db.processor.find()
+    processorcoolers = mongo.db.processorcooler.find()
     memory = mongo.db.memory.find()
-    graphicscard = mongo.db.graphicscard.find()
-    harddrive = mongo.db.harddrive.find()
-    powersupply = mongo.db.powersupply.find()
-    case = mongo.db.case.find()
-    return render_template('/addbuild.html', motherboard=motherboard, processor=processor, processorcooler=processorcooler, memory=memory, graphicscard=graphicscard, harddrive=harddrive, powersupply=powersupply, case=case, build_name=build_name)
+    graphicscards = mongo.db.graphicscard.find()
+    harddrives = mongo.db.harddrive.find()
+    powersupplies = mongo.db.powersupply.find()
+    cases = mongo.db.case.find()
+    return render_template('/addbuild.html', motherboards=motherboards, processors=processors, processorcoolers=processorcoolers, memory=memory, graphicscards=graphicscards, harddrives=harddrives, powersupplies=powersupplies, cases=cases, build_names=build_names)
 
 
 @app.route('/insert_build', methods=['POST'])
@@ -42,6 +42,20 @@ def insert_build():
     build = mongo.db.build
     build.insert_one(request.form.to_dict())
     return redirect(url_for('all_builds'))
+
+
+@app.route('/edit_build/<build_id>')
+def edit_build(build_id):
+    the_build = mongo.db.build.find_one({"_id": ObjectId(build_id)})
+    all_cases = mongo.db.case.find(),
+    all_graphics_cards = mongo.db.graphicscard.find(),
+    all_hard_drives = mongo.db.harddrive.find(),
+    all_memory = mongo.db.memory.find(),
+    all_motherboards = mongo.db.motherboard.find(),
+    all_power_supplies = mongo.db.powersupply.find(),
+    all_processors = mongo.db.processor.find(),
+    all_processor_coolers = mongo.db.processorcooler.find()
+    return render_template('editbuild.html', build=the_build, cases=all_cases, graphicscards=all_graphics_cards, harddrives=all_hard_drives, memory=all_memory, motherboards=all_motherboards, powersupplies=all_power_supplies, processors=all_processors, processorcoolers=all_processor_coolers ) 
 
 
 if __name__ == '__main__':
