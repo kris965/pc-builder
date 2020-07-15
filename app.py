ba73399cfd31ce4ebb5e142bcfbc9d56a1f86230
kris965 <kristian_hoogland@hotmail.com>
2020-07-15 16:44:55 +0000

    set debug to False

--- app.py
+++ app.py
@@ -406,4 +406,4 @@ def search_build_by_case():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
